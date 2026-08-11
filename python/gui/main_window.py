7c54e8854e3956b46a12a56e2a4b923e5a342b13
Jacob Colapietro <jacob.colapietro@vector.com>
2026-08-11 16:05:30 -0400

    Create a gui with a button to generate the config, does nothing currently

--- python/gui/main_window.py
+++ python/gui/main_window.py
@@ -1,14 +1,30 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLineEdit, QLabel, QComboBox
+from PySide6.QtCore import QSize, Qt
 import sys
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
 # If you know you won't use command line arguments QApplication([]) works too.
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PC-lint Plus Configurator")
+        self.setFixedSize(QSize(600, 600))
+
+        button = QPushButton("Generate Config")
+        button.setCheckable(True)
+        button.clicked.connect(self.on_button_clicked)
+        
+        self.setCentralWidget(button)
+
+    def on_button_clicked(self):
+        print("Generate Config button clicked!")
+
 def create_window():
     app = QApplication(sys.argv)
 
     # Create a Qt widget, which will be our window.
-    window = QWidget()
+    window = MainWindow()
     window.show()
 
     app.exec()
